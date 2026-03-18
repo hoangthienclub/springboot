6a535447b04afaea2eeae1e3a4117f858702a37d
Thien Tran <thien301194@gmail.com>
2026-03-19 00:28:48 +0700

    #15 Validation nâng cao, trích xuất thông tin từ annotation

--- src/main/java/com/example/demo/exception/ErrorCode.java
+++ src/main/java/com/example/demo/exception/ErrorCode.java
@@ -10,12 +10,12 @@ public enum ErrorCode {
     INVALID_KEY(1001, "Uncategorized error", HttpStatus.BAD_GATEWAY),
     USER_EXISTED(1002, "User existed", HttpStatus.BAD_GATEWAY),
     USER_NOT_FOUND(1003, "User not found", HttpStatus.BAD_GATEWAY),
-    USERNAME_INVALID(1004, "Username must be at least 3 characters", HttpStatus.BAD_GATEWAY),
-    INVALID_PASSWORD(1005, "Password must be at least 8 characters", HttpStatus.BAD_GATEWAY),
+    USERNAME_INVALID(1004, "Username must be at least {min} characters", HttpStatus.BAD_GATEWAY),
+    INVALID_PASSWORD(1005, "Password must be at least {min} characters", HttpStatus.BAD_GATEWAY),
     USER_NOT_EXIST(1006, "User not exist", HttpStatus.NOT_FOUND),
     UNAUTHENTICATED(1007, "Unauthenticated", HttpStatus.UNAUTHORIZED),
     UNAUTHORIZED(1008, "You do not have permission", HttpStatus.FORBIDDEN),
-    INVALID_DOB(1009, "Invalid date of birth", HttpStatus.BAD_REQUEST),
+    INVALID_DOB(1009, "Your age must be at least {min}", HttpStatus.BAD_REQUEST),
     ;
 
     ErrorCode(int code, String message, HttpStatusCode statusCode) {
